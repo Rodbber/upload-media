16bec91f4e7ca57d367199737e1631e28f2aa765
Rodrigo Barbosa Bertini <rod.lemon@hotmail.com>
2024-02-14 23:56:57 -0300

    feat: create routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,16 @@
 <?php
 
+use App\Http\Controllers\MediaFileController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::group(['prefix' => 'media-files', 'as' => 'media-files.'], function(){
+    Route::get('/', [MediaFileController::class, 'index'])->name('index');
+    Route::post('/', [MediaFileController::class, 'store'])->name('store');
+    Route::put('/{id}', [MediaFileController::class, 'fileRename'])->name('file-rename');
+    Route::delete('/{id}', [MediaFileController::class, 'destroy'])->name('delete');
 });
